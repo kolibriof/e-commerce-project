4fff4335fa28ace7273b71eac7d6adf8d41c9026
kolibriof <kolibri380@gmail.com>
2023-05-15 13:50:36 +0200

    completed the fetch request

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -38,7 +38,7 @@ export const ProductsProvider = ({ children }) => {
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: response.data });
       return response.data;
     } catch (error) {
-      console.log(error);
+      dispatch({ type: GET_PRODUCTS_ERROR });
     }
   };
   useEffect(() => {
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -30,6 +30,9 @@ const products_reducer = (state, action) => {
       featured_products: featured_products,
     };
   }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
   return state;
 };
 
