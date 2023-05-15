0a858997467cfc484be611b2fe017e08735671a8
kolibriof <kolibri380@gmail.com>
2023-05-15 13:02:00 +0200

    Services and Contact components completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 50% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            nemo blanditiis autem unde tempore alias ea, quis perferendis
+            ratione, officia nostrum assumenda velit enim error dignissimos,
+            repudiandae voluptate reprehenderit qui?
+          </p>
+        </div>
+        <form className="contact-form">
+          <input
+            type="email"
+            placeholder="Enter email.."
+            className="form-input"
+          />
+          <button type="submit" className="submit-btn">
+            Subscribe!
+          </button>
+        </form>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +91,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
