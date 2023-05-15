86541cffe5caa229ea7777072ae57f529edb1bb7
kolibriof <kolibri380@gmail.com>
2023-05-15 13:45:58 +0200

    completed fetch products

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,8 +16,21 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (item) => item.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products: featured_products,
+    };
+  }
   return state;
-  throw new Error(`No Matching "${action.type}" - action type`);
 };
 
 export default products_reducer;
