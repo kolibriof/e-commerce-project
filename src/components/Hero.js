252f2ffd3540eab59556fecd2f92459483e47e85
kolibriof <kolibri380@gmail.com>
2023-05-15 12:21:58 +0200

    completed hero on the home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import heroBcg from '../assets/hero-bcg.jpeg'
-import heroBcg2 from '../assets/hero-bcg-2.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import heroBcg from "../assets/hero-bcg.jpeg";
+import heroBcg2 from "../assets/hero-bcg-2.jpeg";
 
 const Hero = () => {
-  return <h4>hero</h4>
-}
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          sint quam vero, architecto exercitationem a aliquam quis sapiente
+          ipsum, placeat mollitia qui minima laudantium enim et. Commodi nemo
+          ratione voluptatibus!
+        </p>
+        <Link to="products" className="btn hero-btn">
+          shop now
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="an image of the table" className="main-img" />
+        <img
+          src={heroBcg2}
+          alt="an image of the person working"
+          className="accent-img"
+        />
+      </article>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   min-height: 60vh;
@@ -58,7 +84,7 @@ const Wrapper = styled.section`
       border-radius: var(--radius);
     }
     .img-container::before {
-      content: '';
+      content: "";
       position: absolute;
       width: 10%;
       height: 80%;
@@ -68,6 +94,6 @@ const Wrapper = styled.section`
       border-radius: var(--radius);
     }
   }
-`
+`;
 
-export default Hero
+export default Hero;
