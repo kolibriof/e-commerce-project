b225495c0ed3a241c937c2a073dc5fe24f9e4c6e
kolibriof <kolibri380@gmail.com>
2023-05-15 10:58:27 +0200

    basic header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter, Switch, Route, Routes } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+import {
+  AboutPage,
+  AuthWrapper,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRoute,
+  ProductPage,
+  SingleProductPage,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route index element={<HomePage />}></Route>
+        <Route path="about" element={<AboutPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="products" element={<ProductPage />} />
+        <Route path="products/:id" element={<SingleProductPage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
