8460d5ddbd607621ae3108469f69043186f42fd7
kolibriof <kolibri380@gmail.com>
2023-05-17 15:48:12 +0200

    single item page completed

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,13 +1,62 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { FaCheck } from 'react-icons/fa'
-import { useCartContext } from '../context/cart_context'
-import AmountButtons from './AmountButtons'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaCheck } from "react-icons/fa";
+import { useCartContext } from "../context/cart_context";
+import AmountButtons from "./AmountButtons";
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
-}
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product;
+  const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+  const increase = () => {
+    if (amount < stock) {
+      setAmount(amount + 1);
+    } else {
+      setAmount(amount);
+    }
+  };
+  const decrease = () => {
+    if (amount > 1) {
+      setAmount(amount - 1);
+    } else {
+      setAmount(amount);
+    }
+  };
+  return (
+    <Wrapper>
+      <div className="colors">
+        <span>colors: </span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                className={
+                  mainColor === color ? `color-btn active` : `color-btn`
+                }
+                style={{ backgroundColor: `${color}` }}
+                onClick={() => setMainColor(colors[index])}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to="/cart" className="btn">
+          add to cart
+        </Link>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   margin-top: 2rem;
@@ -53,5 +102,5 @@ const Wrapper = styled.section`
     margin-top: 1rem;
     width: 140px;
   }
-`
-export default AddToCart
+`;
+export default AddToCart;
