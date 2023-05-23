65fbbc96a8249ef6313f183cf8074e4ed5a8ddf9
kolibriof <kolibri380@gmail.com>
2023-05-23 13:50:26 +0200

    auth0 setup

--- src/index.js
+++ src/index.js
@@ -7,16 +7,30 @@ import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, User } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// dev-2xo4e01pzbwvv8st.us.auth0.com
+// LdYyf3DfdgK56C2SDVJtlE1nNjRGfxkD
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-2xo4e01pzbwvv8st.us.auth0.com"
+    clientId="LdYyf3DfdgK56C2SDVJtlE1nNjRGfxkD
+"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
